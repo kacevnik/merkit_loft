f9fdcc121ef216a6fac3513ca5a8042267a8ea63
kacevnik <kacevnik@yandex.ru>
2017-09-19 01:56:50 +0300

    Дополнительные всплывающие окна

--- js/script.js
+++ js/script.js
@@ -6,7 +6,15 @@ jQuery(document).ready(function ($) {
 	});
 
 	$('#link_call_back').click(function() {
-		$('#call_back input[name="data"]').val('Длина: '+$('#size_table input[name="long"]').val()+' Ширина: '+$('#size_table input[name="width"]').val()+' Высота: '+$('#size_table input[name="height"]').val()+' Тип стола: '+$('#size_table select[name="title_table"]').val())
+		$('#call_back input[name="data"]').val('Длина: '+$('#size_table input[name="long"]').val()+' Ширина: '+$('#size_table input[name="width"]').val()+' Высота: '+$('#size_table input[name="height"]').val()+' Тип стола: '+$('#size_table select[name="title_table"]').val());
+	});	
+
+	$('.sell').click(function() {
+		$('#call_back_sell input[name="data"]').val('Стол: '+$(this).prev().find('h1').html()+' Модель: '+$(this).prev().find('h5').html());
+	});	
+
+	$('.form_input a[data-data]').click(function() {
+		$('#call_back_more input[name="data"]').val($(this).attr('data-data'));
 	});
 
     $("form").ajaxForm(function(){
@@ -25,6 +33,15 @@ jQuery(document).ready(function ($) {
   		nav:true,
   		navText: ['<div class="arrow_prev_owl"></div>', '<div class="arrow_next_owl"></div>']
   	});
+
+  	$("a[href*='#']").bind("click", function(e){
+		var anchor = $(this);
+		$('html, body').stop().animate({
+			scrollTop: $(anchor.attr('href')).offset().top
+		}, 500);
+		e.preventDefault();
+		return false;
+	});
 
   	$("[data-fancybox]").fancybox({
   		padding: '300px'
